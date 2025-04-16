b3b21a7039860c81426f36297a04722f841a8ec6
shahrukhmoin <98784419+shahrukhmoin@users.noreply.github.com>
2025-04-16 14:19:50 +0100

    Update chat_moderator.py

--- chat_moderator.py
+++ chat_moderator.py
@@ -67,7 +67,7 @@ def is_spam(text)
 model, vectorizer = load_model()
 
 # GUI
-st.title(📚 AI Chat Moderator for Schools)
+st.title( AI Chat Moderator for Schools)
 st.write(Detect cyberbullying, threats, profanity, spam, and sentiment in real-time student chat)
 
 user_role = st.selectbox(Select your role, [Student, Admin])
